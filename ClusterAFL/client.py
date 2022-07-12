91e89e742a7f542a69b74799013d48941b77267f
Thais Luca <thais.luca95@gmail.com>
2022-07-12 21:48:01 +0000

    Adding evaluation set

--- ClusterAFL/client.py
+++ ClusterAFL/client.py
@@ -9,23 +9,20 @@ from sklearn.metrics import log_loss
 import utils
 
 class MnistClient(fl.client.NumPyClient):
-    def __init__(self, model, X_train, y_train) -> None:
+    def __init__(self, model, x_train, y_train, x_val, y_val) -> None:
         self.model = model
-        self.X_train, self.y_train = X_train, y_train
+        self.x_train, self.y_train = x_train, y_train
+        self.x_val, self.y_val = x_val, y_val
 
-    def get_parameters(self):  # type: ignore
-        return utils.get_model_parameters(self.model)
+    def get_parameters(self):
+        return self.model.get_weights()
 
-    def fit(self, parameters, config):  # type: ignore
-        utils.set_model_params(self.model, parameters)
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            self.model.fit(self.X_train, self.y_train)
-        print(f"Training finished for round {config['rnd']}")
-        return utils.get_model_parameters(self.model), len(self.X_train), {}
+    def fit(self, parameters, config):
+        self.model.set_weights(parameters)
+        self.model.fit(self.x_train, self.y_train, epochs=1, verbose=2)
+        return self.model.get_weights(), len(self.x_train), {}
 
-    def evaluate(self, parameters, config):  # type: ignore
-        utils.set_model_params(self.model, parameters)
-        loss = log_loss(self.y_test, self.model.predict_proba(X_test))
-        accuracy = self.model.score(self.X_test, self.y_test)
-        return loss, len(self.X_test), {"accuracy": accuracy}
+    def evaluate(self, parameters, config):
+        self.model.set_weights(parameters)
+        loss, acc = self.model.evaluate(self.x_val, self.y_val, verbose=2)
+        return loss, len(self.x_val), {"accuracy": acc}
