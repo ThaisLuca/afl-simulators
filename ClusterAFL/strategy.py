d13de147999bc68929f3171db23696516d61c55e
Thais Luca <thais.luca95@gmail.com>
2022-07-13 12:21:11 +0000

    Testing fuctions and starting to use k-means

--- ClusterAFL/strategy.py
+++ ClusterAFL/strategy.py
@@ -4,6 +4,8 @@ import random
 import flwr as fl
 import numpy as np
 
+from sklearn.cluster import KMeans
+
 from logging import WARNING, INFO, DEBUG
 from typing import Callable, Dict, List, Optional, Tuple
 
@@ -27,23 +29,72 @@ class HalfOfWeightsStrategy(fl.server.strategy.FedAvg):
 
     def __init__(self,min_available_clients): #fraction_fit,fraction_eval,min_fit_clients,min_eval_clients):
 
-        super().__init__(min_available_clients=min_available_clients,
-        #fraction_fit=fraction_fit,
-        #fraction_eval=fraction_eval,
-        #min_fit_clients=min_fit_clients,
-        #min_eval_clients=min_eval_clients,
-        )
+      super().__init__(min_available_clients=min_available_clients,
+      #fraction_fit=fraction_fit,
+      #fraction_eval=fraction_eval,
+      #min_fit_clients=min_fit_clients,
+      #min_eval_clients=min_eval_clients,
+      )
 
     def aggregate_fit(self, rnd, results, failures):
-        """Aggregate fit results using weighted average for half of the clients."""
+      """Aggregate fit results using weighted average for half of the clients."""
         
-        log(INFO, 'Using Half Of Weights Strategy')
-        log(DEBUG, len(results))
+      log(INFO, 'Using Half Of Weights Strategy')
 
-        # Sample half of clients for aggregation
-        half_clients = random.sample(results, int(len(results)/2))
+      # Sample half of clients for aggregation
+      half_clients = random.sample(results, int(len(results)/2))
 
-        log(DEBUG, len(half_clients))
-        return super().aggregate_fit(rnd, results, failures)
+      parameters_aggregated, metrics_aggregated = super().aggregate_fit(rnd, results, failures)
+      log(DEBUG, 'All clients')
+      log(DEBUG, parameters_aggregated)
+
+      half_parameters_aggregated, half_metrics_aggregated = super().aggregate_fit(rnd, half_clients, failures)
+      log(DEBUG, 'Half clients')
+      log(DEBUG, half_parameters_aggregated)
+      
+      return parameters_aggregated, metrics_aggregated
+
+    def aggregate_evaluate(self, rnd, results, failures):
+      """Aggregate evaluation results."""
+
+      # Sample half of clients for aggregation
+      half_clients = random.sample(results, int(len(results)/2))
+
+      log(DEBUG, len(half_clients))
+      return super().aggregate_evaluate(rnd, half_clients, failures)
+
+
+class ClusterStrategy(fl.server.strategy.FedAvg):
+
+    def __init__(self,min_available_clients): #fraction_fit,fraction_eval,min_fit_clients,min_eval_clients):
+
+      super().__init__(min_available_clients=min_available_clients,
+      #fraction_fit=fraction_fit,
+      #fraction_eval=fraction_eval,
+      #min_fit_clients=min_fit_clients,
+      #min_eval_clients=min_eval_clients,
+      )
+
+    def aggregate_fit(self, rnd, results, failures):
+      """Aggregate fit results using weighted average for half of the clients."""
+        
+      log(INFO, 'Using Half Of Weights Strategy')
+      log(DEBUG, len(results))
+
+      # Sample half of clients for aggregation
+      half_clients = random.sample(results, int(len(results)/2))
+
+      log(DEBUG, len(half_clients))
+      return super().aggregate_fit(rnd, half_clients, failures)
+
+    def aggregate_evaluate(self, rnd, results, failures):
+      """Aggregate evaluation results."""
+
+      # Sample half of clients for aggregation
+      half_clients = random.sample(results, int(len(results)/2))
+
+      log(DEBUG, len(half_clients))
+      return super().aggregate_evaluate(rnd, half_clients, failures)
+
 
 
