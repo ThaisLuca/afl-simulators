6afdc5ed14a391e04a281b31a981674f9214ce54
Thais Luca <thais.luca95@gmail.com>
2022-07-13 12:59:11 +0000

    Cleaning file

--- ClusterAFL/strategy.py
+++ ClusterAFL/strategy.py
@@ -29,12 +29,7 @@ class HalfOfWeightsStrategy(fl.server.strategy.FedAvg):
 
     def __init__(self,min_available_clients): #fraction_fit,fraction_eval,min_fit_clients,min_eval_clients):
 
-      super().__init__(min_available_clients=min_available_clients,
-      #fraction_fit=fraction_fit,
-      #fraction_eval=fraction_eval,
-      #min_fit_clients=min_fit_clients,
-      #min_eval_clients=min_eval_clients,
-      )
+      super().__init__(min_available_clients=min_available_clients)
 
     def aggregate_fit(self, rnd, results, failures):
       """Aggregate fit results using weighted average for half of the clients."""
@@ -44,15 +39,9 @@ class HalfOfWeightsStrategy(fl.server.strategy.FedAvg):
       # Sample half of clients for aggregation
       half_clients = random.sample(results, int(len(results)/2))
 
-      parameters_aggregated, metrics_aggregated = super().aggregate_fit(rnd, results, failures)
-      log(DEBUG, 'All clients')
-      log(DEBUG, parameters_aggregated)
-
-      half_parameters_aggregated, half_metrics_aggregated = super().aggregate_fit(rnd, half_clients, failures)
-      log(DEBUG, 'Half clients')
-      log(DEBUG, half_parameters_aggregated)
+      return super().aggregate_fit(rnd, half_clients, failures)
       
-      return parameters_aggregated, metrics_aggregated
+      #return parameters_aggregated, metrics_aggregated
 
     def aggregate_evaluate(self, rnd, results, failures):
       """Aggregate evaluation results."""
