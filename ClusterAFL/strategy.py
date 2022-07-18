ebe266ab224cc48a9f14c0a5e928637a8ed7e297
Thais Luca <thais.luca95@gmail.com>
2022-07-18 21:05:55 +0000

    WIP: metrics evaluation

--- ClusterAFL/strategy.py
+++ ClusterAFL/strategy.py
@@ -49,6 +49,28 @@ class HalfOfWeightsStrategy(fl.server.strategy.FedAvg):
 
       return super().aggregate_evaluate(rnd, half_clients, failures)
 
+    # def aggregate_evaluate(
+    #     self,
+    #     rnd: int,
+    #     results: List[Tuple[ClientProxy, EvaluateRes]],
+    #     failures: List[BaseException],
+    # ) -> Optional[float]:
+    #     """Aggregate evaluation losses using weighted average."""
+    #     if not results:
+    #         return None
+
+    #     # Weigh accuracy of each client by number of examples used
+    #     accuracies = [r.metrics["accuracy"] * r.num_examples for _, r in results]
+    #     examples = [r.num_examples for _, r in results]
+
+    #     # Aggregate and print custom metric
+    #     accuracy_aggregated = sum(accuracies) / sum(examples)
+    #     print(f"Round {rnd} accuracy aggregated from client results: {accuracy_aggregated}")
+
+    #     # Call aggregate_evaluate from base class (FedAvg)
+    #     return super().aggregate_evaluate(rnd, results, failures)
+
+
 class ClusterStrategy(fl.server.strategy.FedAvg):
 
     def __init__(self, min_available_clients):
