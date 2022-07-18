d2035b151e7fd40b8b29f06a0ccd320242980540
Thais Luca <thais.luca95@gmail.com>
2022-07-18 21:05:11 +0000

    Setting to PyTorch, fixing errors

--- ClusterAFL/client.py
+++ ClusterAFL/client.py
@@ -3,60 +3,19 @@ import torch
 import warnings
 import flwr as fl
 import numpy as np
-import tensorflow as tf
 
-from collections import OrderedDict
 from collections import OrderedDict
 from typing import List
 
-DEVICE = torch.device("cpu")
+from utils import train, test
 
 class FlowerClient(fl.client.NumPyClient):
-    def __init__(self, ID, model, trainloader, valloader):
+    def __init__(self, ID, model, trainloader, valloader, testloader):
         self.ID = ID
         self.model = model
         self.trainloader = trainloader
         self.valloader = valloader
-
-    def train(self, epochs: int, verbose=False):
-      """Train the network on the training set."""
-      criterion = torch.nn.CrossEntropyLoss()
-      optimizer = torch.optim.Adam(self.model.parameters())
-      self.model.train()
-      for epoch in range(epochs):
-          correct, total, epoch_loss = 0, 0, 0.0
-          for images, labels in self.trainloader:
-              images, labels = images.to(DEVICE), labels.to(DEVICE)
-              optimizer.zero_grad()
-              outputs = self.model(images)
-              loss = criterion(self.model(images), labels)
-              loss.backward()
-              optimizer.step()
-              # Metrics
-              epoch_loss += loss
-              total += labels.size(0)
-              correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
-          epoch_loss /= len(self.testloader.dataset)
-          epoch_acc = correct / total
-          if verbose:
-              print(f"Epoch {epoch+1}: train loss {epoch_loss}, accuracy {epoch_acc}")
-
-    def test(self):
-      """Evaluate the network on the entire test set."""
-      criterion = torch.nn.CrossEntropyLoss()
-      correct, total, loss = 0, 0, 0.0
-      self.net.eval()
-      with torch.no_grad():
-          for images, labels in self.valloader:
-              images, labels = images.to(DEVICE), labels.to(DEVICE)
-              outputs = self.model(images)
-              loss += criterion(outputs, labels).item()
-              _, predicted = torch.max(outputs.data, 1)
-              total += labels.size(0)
-              correct += (predicted == labels).sum().item()
-      loss /= len(self.testloader.dataset)
-      accuracy = correct / total
-      return loss, accuracy
+        self.testloader = testloader
 
     def get_parameters(self) -> List[np.ndarray]:
       return [val.cpu().numpy() for _, val in self.model.state_dict().items()]
@@ -68,10 +27,10 @@ class FlowerClient(fl.client.NumPyClient):
 
     def fit(self, parameters, config):
         self.set_parameters(parameters)
-        self.train(epochs=1, verbose=True)
+        train(self.model, self.trainloader, self.valloader, epochs=1, verbose=True)
         return self.get_parameters(), len(self.trainloader), {}
 
     def evaluate(self, parameters, config):
         self.set_parameters(parameters)
-        loss, accuracy = self.test()
+        loss, accuracy = test(self.model, self.testloader)
         return float(loss), len(self.valloader), {"accuracy": float(accuracy)}
--- ClusterAFL/main.py
+++ ClusterAFL/main.py
@@ -1,10 +1,11 @@
 
 from collections import OrderedDict
-from typing import List
+from typing import List, Tuple, Optional
 
 import flwr as fl
 import numpy as np
 import matplotlib.pyplot as plt
+
 import torch
 import torch.nn as nn
 import torchvision
@@ -14,21 +15,40 @@ from torch.utils.data import DataLoader, random_split
 from torchvision.datasets import CIFAR10
 
 from flwr.common.logger import log
-from client import FlowerClient, DEVICE
+from client import FlowerClient
 from model import Net
-
-NUM_CLIENTS = 100
-NUM_ROUNDS = 3
-BATCH_SIZE = 32
-MIN_AVAILABLE_CLIENTS = 2 # int(NUM_CLIENTS * 0.75)# Wait until at least 75 clients are available
-#FRACTION_FIT = 0.1                                # Sample 10% of available clients for training
-#MIN_FIT_CLIENTS = 10                              # Never sample less than 10 clients for training
-#FRACTION_EVAL = 0.05                              # Sample 5% of available clients for evaluation
-#MIN_EVAL_CLIENTS = 5                              # Never sample less than 5 clients for evaluation
+from utils import *
 
 def fit_round(rnd: int):
     """Send round number to client."""
     return {"rnd": rnd}
+
+def evaluate_config(rnd: int):
+  """Return evaluation configuration dict for each round.
+  Perform five local evaluation steps on each client (i.e., use five
+  batches) during rounds one to three, then increase to ten local
+  evaluation steps.
+  """
+  val_steps = 5 if rnd < 4 else 10
+  return {"val_steps": val_steps}
+
+
+def get_eval_fn(model):
+  """Return an evaluation function for server-side evaluation."""
+
+  # Download and transform CIFAR-10 (train and test)
+  transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+  )
+  testset = CIFAR10("./dataset", train=False, download=True, transform=transform)
+  testloader = DataLoader(testset)
+
+  # The `evaluate` function will be called after every round
+  def evaluate(weights: fl.common.Weights) -> Optional[Tuple[float, float]]:
+    set_parameters(model, weights) # Update model with the latest parameters
+    loss, accuracy = test(model, testloader)
+    return float(loss), {"accuracy": float(accuracy)}
+  return evaluate
 
 def load_datasets():
     # Download and transform CIFAR-10 (train and test)
@@ -56,6 +76,7 @@ def load_datasets():
     testloader = DataLoader(testset, batch_size=BATCH_SIZE)
     return trainloaders, valloaders, testloader
 
+
 def client_fn(cid: str) -> FlowerClient:
 
     # Create model
@@ -70,12 +91,18 @@ def client_fn(cid: str) -> FlowerClient:
     valloader = valloaders[int(cid)]
 
     # Create a  single Flower client representing a single organization
-    return FlowerClient(cid, model, trainloader, valloader)
+    return FlowerClient(cid, model, trainloader, valloader, testloader)
 
 # Start Flower server for NUM_ROUNDS rounds of federated learning
 if __name__ == "__main__":
+
+    # Creates server's model
+    model = Net().to(DEVICE)
+
     #fl.server.strategy.FedAvg
     strategy = fl.server.strategy.FedAvg(min_available_clients=MIN_AVAILABLE_CLIENTS,
+        on_evaluate_config_fn=evaluate_config,
+        eval_fn=get_eval_fn(model),
         #evaluate_metrics_aggregation_fn=agg
         #on_fit_config_fn=fit_round,
         #fraction_fit=FRACTION_FIT,
